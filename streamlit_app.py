7b98556e1486d4ddeaa26586ac6392db6d651e4a
Karthikbhimarasetti <130474346+Karthikbhimarasetti@users.noreply.github.com>
2023-04-11 22:14:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ st.title('My Parents New Healthy Diner')
 st.Header('Breakfast Menu')
 st.text('Omega 3 and blueberry oatmeal')
 st.text('Kale, Spinach & rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+st.text('Hard-Boiled Free-Range Egg') 
 
