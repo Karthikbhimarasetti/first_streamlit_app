5325648eabffcd4bb0cb27ace3a2068b7d741105
Karthikbhimarasetti <130474346+Karthikbhimarasetti@users.noreply.github.com>
2023-05-02 18:37:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas
 import requests
+pip install snowflake-connector-python
 import snowflake.connector
 
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
